af745a74385e4face20b6f0ac2aad45d699c617d
Shahana Kabir <shahana.kabir@gmail.com>
2021-08-17 10:58:35 -0700

    trying some javascripts methods from udemy course

---
+++ app.js
@@ -0,0 +1,23 @@
+const bestSongs = [
+    {
+        artist: "Bob dylan",
+        song: "I love u",
+    },
+   
+    {
+        artist: "Chuck berry",
+        song: "I play games",
+    },
+
+    {
+        artist: "The Beatles",
+        song: "Hey Jude",
+    },
+    {
+        artist: "Ray Charles",
+        song: "What do I do",
+    },
+
+];
+
+bestSongs.forEach((bestsong) => console.log(bestsong));
