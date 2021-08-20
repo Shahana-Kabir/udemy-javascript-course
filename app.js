0527ebee9315642326457a21779ceefc922f0ebd
Shahana Kabir <shahana.kabir@gmail.com>
2021-08-20 12:41:56 -0700

    this and new keyword concepts

--- app.js
+++ app.js
@@ -10,19 +10,27 @@
 
 //////////
 
-function movieFunc (movieDesc){
+// function movieFunc (movieDesc){
+//     console.log(this.movie, movieDesc);
+// }
+// let moveieSHop1 = {
+//     movie: "alice",
+    
+// };
+// let moveieSHop2 = {
+//     movie: "bob",
+    
+// };
+
+// movieFunc.call(moveieSHop1, "is a fantastic movie");
+// movieFunc.call(moveieSHop2, "is a fantastic movie");
+
+function movieFunc(movieDesc){
     console.log(this.movie, movieDesc);
 }
-let moveieSHop1 = {
-    movie: "click",
-    movieFunc: movieFunc,
-};
-let moveieSHop2 = {
-    movie: "click2",
-    movieFunc: movieFunc,
-};
 
-moveieSHop1.movieFunc("click is a fantastic movie");
-moveieSHop2.movieFunc("click2 is a fantastic movie");
-
+let newEmptyObject = new movieFunc("oops");
+newEmptyObject.name = 'Shahana';
+newEmptyObject.age = 37;
+console.log(newEmptyObject);
 
