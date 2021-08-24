c3316f5659517b9ca00b98f5c73400be7dd09380
Shahana Kabir <shahana.kabir@gmail.com>
2021-08-24 12:39:52 -0700

    Asynchronous programming

--- app.js
+++ app.js
@@ -25,12 +25,74 @@
 // movieFunc.call(moveieSHop1, "is a fantastic movie");
 // movieFunc.call(moveieSHop2, "is a fantastic movie");
 
-function movieFunc(movieDesc){
-    console.log(this.movie, movieDesc);
+// function movieFunc(movieDesc){
+//     console.log(this.movie, movieDesc);
+// }
+
+// let newEmptyObject = new movieFunc("oops");
+// newEmptyObject.name = 'Shahana';
+// newEmptyObject.age = 37;
+// console.log(newEmptyObject);
+
+// var movie = 'rush';
+// function movieFunc(movieDesc){
+//     console.log(movie, movieDesc);
+// }
+
+// movieFunc('this is a racing movie');
+
+let movieShop = {
+    movie: "Hell or high water",
+    movieFunc(movieDesc){
+        console.log(this.movie, movieDesc);
+    }
+};
+// movieShop.movieFunc("is a funny movie");
+
+class Workshop{
+    constructor(teacher){
+        this.teacher = teacher;
+    }
+    ask(question){
+        console.log(this.teacher, question);
+    }
+}
+class AnotherWorkshop extends Workshop{
+    speakUp(msg){
+        this.ask(msg);
+    }
+}
+let python = new AnotherWorkshop("john");
+// 
+////// Asynchronous programming////
+function hello(){
+    console.log("hello");
+}
+// setTimeout(hello, 1000);
+// console.log("hello again");
+////// The call stack in the browser////
+
+divide(100,5);
+
+function multiply(x,y){
+    const result = x*y;
+    console.log(result);
 }
 
-let newEmptyObject = new movieFunc("oops");
-newEmptyObject.name = 'Shahana';
-newEmptyObject.age = 37;
-console.log(newEmptyObject);
+function sum(x,y){
+    const result = x+y;
+    console.log(result);
+    multiply(20,30);
+}
 
+function subtract(x,y){
+    const result = x-y;
+    console.log(result);
+    sum(30,50);
+}
+
+function divide(x,y){
+    const result = x/y;
+    console.log(result);
+    subtract(50,40);
+}
